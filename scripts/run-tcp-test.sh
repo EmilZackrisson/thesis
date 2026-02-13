d1c5a0fee9bc46b58a760ee25d559dc7fc5476ed
Emil Zackrisson <emil.zackrisson@gmail.com>
2026-02-13 15:25:23 +0100

    fix script

--- scripts/run-tcp-test.sh
+++ scripts/run-tcp-test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "Start minikube tunnel"
-minikube tunnel --bind-address='10.200.200.1' &
-tunnel_pid=$!
-echo "Tunnel running with PID=${tunnel_pid}"
+# This is made to run on NodeB (casual-lamb) only
 
 echo "Deploying tcpecho"
 kubectl apply -f https://github.com/EmilZackrisson/thesis/blob/main/K8s/tcpecho/deployment.yaml
@@ -18,10 +15,5 @@ tcpserver -e $EXP_ID -r $RUN_ID -k $KEY_ID -p 9000 &
 echo "Starting tcpclient"
 tcpclient -e $EXP_ID -r $RUN_ID -k $KEY_ID --server 10.200.200.1 -p 9000 -n 100 --pktsize u --wait u
 
-echo "Killing tunnel"
-trap 'kill -15 ${tunnel_pid}'; echo "killed: ${tunnel_pid}"; INT
-
-echo "Waiting for kills..."
-wait
 
 echo "SUCCESS"
