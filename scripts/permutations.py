c387b7a3ea43ee989e5bc4dbfbc13115fa19271b
Emil Zackrisson <emil.zackrisson@gmail.com>
2026-03-23 13:59:01 +0100

    add logging suffix to permutations script

--- scripts/permutations.py
+++ scripts/permutations.py
@@ -15,6 +15,8 @@ permutations = list(product(*lists))
 
 passed = []
 
+suffix = "2>&1 | tee -a /var/log/k8test-ntas.log"
+
 for perm in permutations:
     if perm[1] == 'udp' and perm[3] != 'no':
         pass
@@ -30,4 +32,4 @@ for perm in permutations:
         passed.append(' '.join(perm))
 
 for perm in passed:
-    print(f"{SCRIPTPATH} {perm} {REPOPATH}")
+    print(f"{SCRIPTPATH} {perm} {REPOPATH} {suffix}")
