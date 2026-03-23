aa2485be5a7f9dc4091986e02ea078ecbe2633a4
Emil Zackrisson <emil.zackrisson@gmail.com>
2026-03-23 11:18:08 +0100

    fix: remove spaces around assignment operators in run-http-test.sh

--- scripts/run-http-test.sh
+++ scripts/run-http-test.sh
@@ -3,10 +3,10 @@
 # This is made to run on NodeB (casual-lamb) only
 
 echo "Running httptrafficgenerator"
-PKT_COUNT = 1000
-MIN_SIZE = 200
-MAX_SIZE = 1500
-MIN_INT_MS = 1
-MAX_INT_MS = 1000
-DEST_URL = http://10.200.200.1:30001
+PKT_COUNT=1000
+MIN_SIZE=200
+MAX_SIZE=1500
+MIN_INT_MS=1
+MAX_INT_MS=1000
+DEST_URL=http://10.200.200.1:30001
 httptrafficgenerator $PKT_COUNT $MIN_SIZE $MAX_SIZE $MIN_INT_MS $MAX_INT_MS $DEST_URL
