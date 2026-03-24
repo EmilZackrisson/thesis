67ee9034aaf88f5d4ff0ec674a6b25321b177a41
Emil Zackrisson <emil.zackrisson@gmail.com>
2026-03-24 10:24:17 +0100

    test ingress policy

--- scripts/delete-cluster.sh
+++ scripts/delete-cluster.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-sudo kubeadm reset -f
-sudo iptables -F
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
+sudo kubeadm reset -f --cri-socket="unix:///var/run/crio/crio.sock"
+sudo rm -rf /etc/cni/net.d
+docker run --privileged --rm registry.k8s.io/kube-proxy:v1.35.0 sh -c "kube-proxy --cleanup && echo DONE"
 sudo ip link delete cni0
 sudo ip link delete flannel.1   # if exists
-sudo rm -rf /etc/cni/net.d
 sudo rm -rf /var/lib/cni/
 
-echo "Removing all Calico iptable rules"
-for i in `iptables -L |grep cali|awk '{print $2}'`; do iptables -F $i && iptables -X $i; done
+echo "Removing all Kube iptable rules"
+for i in `sudo iptables -L |grep KUBE|awk '{print $2}'`; do sudo iptables -F $i && sudo iptables -X $i; done
